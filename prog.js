acd6fc1f4da3de40a4afa1de3108a4f8697459f1
Erich Abbott <abbotterich@gmail.com>
2015-09-09 19:37:06 -0500

    pseudocode lite html work

--- prog.js
+++ prog.js
@@ -2,17 +2,37 @@ $(function()  {
 
 
 	$('.button').click(function(){
-	    if ($('#r1c1').prop('checked'))
-	    {
-	      alert($(this).val());
-
-	    }else if ($('#r1c2').prop('checked')) 
-	    {
-	    	alert($(this).val());
-	    }
+		
+		alert( $(this).attr('id') )
   });
 });
 
 
+// var playerOne == false
+// var playerTwo == false 
+
+//while (playerOne, playerTwo == false) {
+
+	//run function findWinner;
+//}
+//winner == true
+// 	function winConditions()
+// var topRow = r1c1, r1c2, r1c3
+// var midRow = r2c1, r2c2, r2c3
+// var bottomRow = r3c1, r3c2, r3c3
+// var leftCollum = r1c1, r2c1, r3c1
+// var midCollum = r1c2, r2c2, r3c2
+// var rightCollum = r1c3, r2c3, r3c3
+// //
+	//var eitherPlayer = (playerOne || playerTwo) 
+//     function findWinner() {
+// 	if (eitherPlayer = winConditions) {
+// 		winner
+		//alert('Boomshakalaka!')
+// 	} 
+// } 
+
+
+//}
 
 
